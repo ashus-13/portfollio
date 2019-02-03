5f03cd7d1f6a38635f7bcd8e8a30f7471ae89ac5
ashus-13 <ashutosh.soccer@gmail.com>
2019-02-03 11:02:26 +0530

    Bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+	place = models.CharField(max_length=30)
+	date = models.DateTimeField()
+	activity = models.TextField()
--- porfolio/settings.py
+++ porfolio/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jobs.apps.JobsConfig'
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,12 +77,19 @@ WSGI_APPLICATION = 'porfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'postgres',
+        'PASSWORD': 'django123',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
-
+#'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
