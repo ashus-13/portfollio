b1ed2e656063a302df25d38986c29d1e02097e80
ashus-13 <ashutosh.soccer@gmail.com>
2019-02-03 17:04:10 +0530

    Done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,12 @@ class Blog(models.Model):
 	place = models.CharField(max_length=30)
 	date = models.DateTimeField()
 	activity = models.TextField()
+
+	def __str__(self):
+		return self.place
+
+	def date_new(self):
+		return self.date.strftime('%b %e %Y')
+
+	def activity_new(self):
+		return self.activity[:100]		
